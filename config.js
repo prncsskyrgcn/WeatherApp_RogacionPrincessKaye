62b5c762dd80b2cc3c884888b0bae5f0b278ae8b
Princess Kaye Rogacion <princessrogacion148@gmail.com>
2022-12-28 16:10:29 +0800

    Update

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "4ec5c0904e05f548051f556cad0e94b8&fbclid"
+    }
+}
+
+module.exports = constants;
